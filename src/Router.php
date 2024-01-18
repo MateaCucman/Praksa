4326412ec81983bb449e2427b4224f17121a8a32
MateaCucman <mcucman1@mathos.hr>
2024-01-18 18:36:46 +0100

    Placeholder update

--- src/Router.php
+++ src/Router.php
@@ -19,8 +19,17 @@ class Router
     {
         foreach (self::$routes as $route) 
         {
-            if (($route['method'] === $request->getMethod()) && ('/Praksa' . $route['url'] === $request->getUri())) {
-                return call_user_func($route['cb'], $request);
+            $uri = $request->getUri();
+            $uriParts = explode('/', $uri);
+            $uriLast = $uriParts[sizeof($uriParts)-1];
+            $urlParts = explode('/', $route['url']);
+            if(strpos(':', $urlParts[sizeof($urlParts)-1])==0){
+                $urlParts[sizeof($urlParts)-1]  = str_replace($urlParts[sizeof($urlParts)-1], $uriLast, $urlParts[sizeof($urlParts)-1]);
+            }
+            $urlNew = implode('/', $urlParts);
+            $route['url'] = $urlNew;
+            if (($route['method'] === $request->getMethod()) && ('/Praksa' . $route['url'] === $uri)) {
+                return call_user_func($route['cb'], $request, $uriLast);
                 
             }
         }
